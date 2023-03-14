362c592972ce4730f8dbdd13d4da4dabb0b4bf92
AdiPlay <84020944+AdiPlay@users.noreply.github.com>
2023-03-14 13:52:26 +0100

    #

--- pandas.py
+++ pandas.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 step_data = [3620, 7891, 9761, 3907, 4338, 5357]
 
-step_counts = pd.Series(step_data, name='steps')
+#step_counts = pd.Series(step_data, name='steps')
+
+step_counts.index = pd.date_range('20150329', periods=6)
 
 print(step_counts)
